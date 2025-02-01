031c48a7cd5e45b7c940ee177f049d27df3ba8a0
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-02-01 13:16:06 -0300

    Creating create and update DTO

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,38 +1,67 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 
-@Controller("users")
+@Controller('users')
 export class UserController {
+  @Post()
+  async create(@Body() { email, name, password }: CreateUserDTO) {
+    return {
+      email,
+      name,
+      password,
+    };
+  }
 
-    @Post()
-    async create(@Body() body) {
-        return {body}
-    }
+  @Get()
+  async read() {
+    return {
+      users: [],
+    };
+  }
 
-    @Get()
-    async read() {
-        return {
-            users: []
-        }
-    }
+  @Get(':id')
+  async readOne(@Param() params) {
+    return {
+      user: {},
+      params,
+    };
+  }
 
-    @Get(":id")
-    async readOne(@Param() params) {
-        return {
-            user: {}, params
-        }
-    }
+  @Put(':id')
+  async fullUpdate(
+    @Body() { email, name, password }: UpdatePutUserDTO,
+    @Param() params,
+  ) {
+    return {
+      body: {
+        email,
+        name,
+        password,
+      },
+      params,
+    };
+  }
+  @Patch(':id')
+  async partialUpdate(
+    @Body() { email, name, password }: UpdatePatchUserDTO,
+    @Param() params,
+  ) {
+    return { body: email, name, password, params };
+  }
 
-    @Put(":id")
-    async fullUpdate(@Body() body, @Param() params){
-        return {body, params}
-    }
-    @Patch(":id")
-    async partialUpdate(@Body() body,@Param() params){
-        return {body, params}
-    }
-
-    @Delete(":id")
-    async delete(@Param() params){
-        return { params}
-    }
+  @Delete(':id')
+  async delete(@Param() params) {
+    return { params };
+  }
 }
