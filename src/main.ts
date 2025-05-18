f66b67014ffcffdf797e8ef296988d983a9b75ab
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-05-18 16:49:10 -0300

    chore: defining node version

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from "cookie-parser"
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser())
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
     origin: true,
-    credentials: true, 
+    credentials: true,
   });
-  
+
   await app.listen(process.env.PORT ?? 3000);
-  
 }
 bootstrap();
